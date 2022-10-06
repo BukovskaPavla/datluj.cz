ba0e183daead02c9975b460b51b6685425554d35
BukovskaPavla <bukovskapavla@gmail.com>
2022-10-06 20:13:45 +0200

    zadani 1

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 const Wordbox = ({ word }) => {
   const [lettersLeft, setLettersLeft] = useState(word);  
+
+  const handleKeyUp = (e) => {
+    
+    if (e.code.substring(3,4).toLowerCase() === lettersLeft.substring(0,1)){
+      setLettersLeft(lettersLeft.substring(1))
+    }
+    }
+
+    useEffect(() => {
+      document.addEventListener("keyup", handleKeyUp)
+      return () => document.removeEventListener('keyup', handleKeyUp);
+    }, [lettersLeft]);
+  
   
   return (
     <div className="wordbox">{lettersLeft}</div>
